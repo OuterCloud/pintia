33f37aa8fb595a28ec25425aa63b9cc57c3f4dcc
zenkilan <zenkilan@tencent.com>
2021-03-25 14:54:52 +0800

    将输入的任意n个整数从小到大输出

--- three_numbers_sort.go
+++ three_numbers_sort.go
@@ -1,8 +1,65 @@
 package pintia
 
-import(
+import (
+	"strconv"
 	"strings"
 )
-func ThreeNumbersSort(threeNumsStr string){
-	strings.Split(threeNumsStr, " ")
+
+// SortedNumber 具有排序属性的数字结构
+type SortedNumber struct {
+	Self  int
+	Left  *SortedNumber
+	Right *SortedNumber
+}
+
+var sortedNumbers []string
+
+// NumbersSort 将输入的任意n个整数从小到大输出
+// 输入格式:输入在一行中给出n个整数，其间以空格分隔
+// 输出格式:在一行中将n个整数从小到大输出，其间以“->”相连
+func NumbersSort(threeNumsStr string) (sortedNumbersStr string) {
+	nums := strings.Split(threeNumsStr, " ")
+	intNumber, _ := strconv.Atoi(nums[0])
+	sortedNumber := SortedNumber{Self: intNumber}
+	for i := 1; i < len(nums); i++ {
+		intNumber, _ := strconv.Atoi(nums[i])
+		InsertNumber(&sortedNumber, intNumber)
+	}
+	PrintSortedNumber(sortedNumber)
+	sortedNumbersStr = strings.Join(sortedNumbers, "->")
+	return
+}
+
+// PrintSortedNumber 打印已排序数字
+func PrintSortedNumber(sortedNumber SortedNumber) {
+	if sortedNumber.Left != nil {
+		PrintSortedNumber(*sortedNumber.Left)
+	}
+	sortedNumbers = append(sortedNumbers, strconv.Itoa(sortedNumber.Self))
+	if sortedNumber.Right != nil {
+		PrintSortedNumber(*sortedNumber.Right)
+	}
+}
+
+// InsertNumber 插入数字
+func InsertNumber(sortedNumber *SortedNumber, intNumber int) {
+	if intNumber >= sortedNumber.Self {
+		// fmt.Printf("%+v大于%+v\n", intNumber, sortedNumber.Self)
+		if sortedNumber.Right == nil {
+			sortedNumber.Right = &SortedNumber{
+				Self: intNumber,
+			}
+		} else {
+			InsertNumber(sortedNumber.Right, intNumber)
+		}
+	} else {
+		// fmt.Printf("%+v小于%+v\n", intNumber, sortedNumber.Self)
+		if sortedNumber.Left == nil {
+			sortedNumber.Left = &SortedNumber{
+				Self: intNumber,
+			}
+		} else {
+			InsertNumber(sortedNumber.Left, intNumber)
+		}
+	}
 }
--- three_numbers_sort_test.go
+++ three_numbers_sort_test.go
@@ -2,8 +2,18 @@ package pintia
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestThreeNumbersSort(t *testing.T) {
-	
+	// 准备待测数据
+	var numbersStr = "-1 -4 -2 9 -999 8 0 6 4 5 3 2 1 7 8 9 9 100"
+	var expectStr = "-999->-4->-2->-1->0->1->2->3->4->5->6->7->8->8->9->9->9->100"
+	// 执行待测方法
+	sortedNumberStr := NumbersSort(numbersStr)
+	t.Log(sortedNumberStr)
+	// 断言
+	assert := assert.New(t)
+	assert.Equal(expectStr, sortedNumberStr, "排序错误")
 }
