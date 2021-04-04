2ec2d80243785599c59f515c5274e8051ef90f36
zenkilan <zenkilan@tencent.com>
2021-04-04 17:26:30 +0800

    增加电梯调度算法和测试

--- exec_test.sh
+++ exec_test.sh
@@ -1,11 +1,20 @@
 method=$1
+# 组装测试命令
 fileList=`ls`
-cmd="go test -v"
+cmd="go test"
 for file in $fileList
 do
     if echo "$file" | grep -q -E '\.go$';then
         cmd="${cmd} $file"
     fi
 done
-cmd="$cmd -test.run $method -gcflags=-l"
+if [ ! -n "$1" ];then
+echo "未指定测试方法"
+else
+cmd="$cmd -test.run $method"
+fi
+cmd="$cmd -v -gcflags=-l -coverprofile=test.out"
+# 执行测试命令
 $cmd
+# 生成覆盖率报告
+go tool cover -html=test.out -o test.html
